9b4afd60ddec1a0910f797203af61f60313a366e
holesachin <holesachin007@gmail.com>
2023-11-05 19:20:22 +0530

    connected to mongo database

---
+++ src/db/index.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constants.js";
+
+async function connectDB() {
+  try {
+    const connectionInstance = await mongoose.connect(
+      `${process.env.MONGODB_URL}/${DB_NAME}`
+    );
+    console.log(
+      `\n MongoDB connected !! DB HOST: ${connectionInstance.connection.host}`
+    );
+  } catch (err) {
+    console.error("DATABASE_CONNECTION_FAILED: ", err);
+    process.exit(1);
+  }
+}
+
+export default connectDB;
